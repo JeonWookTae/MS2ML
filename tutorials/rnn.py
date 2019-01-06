411cf886d26d691898fdce40db9231d6b5bc2ce9
JeonWookTae <pkgg011@naver.com>
2019-01-06 10:18:26 +0900

    tutorials

--- tutorials/rnn.py
+++ tutorials/rnn.py
@@ -13,4 +13,17 @@ def get_data(data_dir):
     return input_data.read_data_sets(train_dir=data_dir, one_hot=True)
 
 
-LOG_DIR = "logs/RNN_with_summaries"
+LOG_DIR = r"../data/logs/RNN_with_summaries"
+DATA_DIR = r'../data/mnist'
+
+time_steps = 28
+element_size = 28
+num_classes = 10
+batch_size = 128
+hidden_size = 128
+data = get_data(data_dir=DATA_DIR)
+_inputs = tf.placeholder(tf.float32, shape=[None, time_steps, element_size], name='inputs')
+y = tf.placeholder(tf.float32, shape=[None, num_classes], name='labels')
+
+batch_x, batch_y = data.train.next_batch(batch_size)
+batch_x = batch_x.reshape((batch_size, time_steps, element_size))
