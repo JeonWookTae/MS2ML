82fe5a0dc383e65706bfeaa887de7ce5cb698d28
JeonWookTae <pkgg011@naver.com>
2019-01-12 09:49:10 +0900

    tutorials

--- tutorials/txt_rnn.py
+++ tutorials/txt_rnn.py
@@ -20,6 +20,7 @@ for i in range(10000):
     rand_seq_len = np.random.choice(range(3, 7))
     seqlens.append(rand_seq_len)
     rand_odd_ints = np.random.choice(range(1, 10, 2), rand_seq_len)
+    rand_even_ints = np.random.choice(range(2, 10, 2), rand_seq_len)
 
     if rand_seq_len < 6:
         rand_odd_ints = np.append(rand_odd_ints, [0] * (6 - rand_seq_len))
@@ -42,10 +43,10 @@ for sent in data:
 index2word_map = {index: word for word, index in word2index_map.items()}
 vocabulary_size = len(index2word_map)
 
-labels = [1]*10000 + [0]*10000
+labels = [1] * 10000 + [0] * 10000
 for i in range(len(labels)):
     labels = labels[i]
-    one_hot_encoding = [0]*2
+    one_hot_encoding = [0] * 2
     one_hot_encoding[labels] = 1
     labels[i] = one_hot_encoding
 
@@ -62,3 +63,50 @@ train_seqlens = seqlens[:10000]
 test_x = data[10000:]
 test_y = labels[10000:]
 test_seqlens = seqlens[10000:]
+
+
+def get_sentence_batch(batch_size, data_x, data_y, data_seqlens):
+    instance_indices = list(range(len(data_x)))
+    np.random.shuffle(instance_indices)
+    batch = instance_indices[:batch_size]
+    x = [[word2index_map[word] for word in data_x[i].lower().split()]
+         for i in batch]
+    y = [data_y[i] for i in batch]
+    seqlens = [data_seqlens[i] for i in batch]
+    return x, y, seqlens
+
+
+_inputs = tf.placeholder(tf.int32, shape=[batch_size, times_steps])
+_labels = tf.placeholder(tf.float32, shape=[batch_size, num_classes])
+_seqlens = tf.placeholder(tf.int32, shape=[batch_size])
+
+with tf.name_scope("embedings"):
+    embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_dimension], -1.0, 1.0), name="embedding")
+    embed = tf.nn.embedding_lookup(embeddings, _inputs)
+
+with tf.variable_scope('lstm'):
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_layer, forget_bias=1.0)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, embed, sequence_length=_seqlens, dtype=tf.float32)
+
+weight = {"linear_layer": tf.Variable(tf.truncated_normal([hidden_layer, num_classes], mean=0, stddev=.01))}
+biases = {"linear_layer": tf.Variable(tf.truncated_normal([num_classes], mean=0, stddev=0.01))}
+
+final_output = tf.matmul(states[1], weight["linear_layer"]) + biases["linear_layer"]
+softmax = tf.nn.softmax_cross_entropy_with_logits_v2(logits= final_output, labels= _labels)
+cross_entropy = tf.reduce_mean(softmax)
+
+train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(_labels, 1), tf.argmax(final_output,1))
+accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32))) * 100
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(1000):
+        x_batch, y_batch, seqlen_batch = get_sentence_batch(batch_size, train_x, train_y, train_seqlens)
+        feed_dict = {_inputs: x_batch, _labels: y_batch, _seqlens:seqlen_batch}
+        sess.run(train_step, feed_dict=feed_dict)
+
+        if step % 100 == 0:
+            acc = sess.run(accuracy, feed_dict=feed_dict)
+            print("accuracy at %d: %.5f"%(step, acc))
